2c5d4e6b00d087f684ebc4795080e9c2091e90a5
Vorono4ka <54549682+Vorono4ka@users.noreply.github.com>
2020-08-27 10:20:46 +0400

    Scw-Tool

---
+++ main.py
@@ -0,0 +1,108 @@
+import os
+
+
+def get_basename(filename: str):
+    filename = filename.split('.')[:-1]
+    basename = ''.join(filename)
+    return basename
+
+
+def make_dir(directory_path: str):
+    if not os.path.exists(directory_path):
+        os.mkdir(directory_path)
+
+
+def _(*args):
+    print('[SCW Tool] ', end='')
+    for arg in args:
+        print(arg, end=' ')
+    print()
+
+
+def _i(text):
+    return input(f'[SCW Tool] {text}: ')
+
+
+makedir = mkdir = make_dir
+
+tools = [
+    'scw2scw',
+    'scw2obj',
+    'scw2dae',
+    'obj2scw',
+    'dae2scw',
+]
+
+if __name__ == '__main__':
+    for tool in tools:
+        _(f'{tools.index(tool)} - {tool}')
+
+    # <SelectTool>
+    tool_index = None
+    while not (tool_index in range(len(tools))):
+        tool_index = _i('Select Tool')
+        try:
+            tool_index = int(tool_index)
+        except ValueError:
+            _('Prompted Value isn\'t integer!')
+    # </SelectTool>
+
+    tool = tools[tool_index]
+
+    _from = tool.split('2')[0]
+    _to = tool.split('2')[1]
+    mkdir(_from)
+    mkdir(_to)
+
+    for file in os.listdir(_from):
+        with open(f'{_from}/{file}', 'rb') as fh:
+            file_data = fh.read()
+            fh.close()
+        base_name = get_basename(file)
+        if _from == 'scw' and _to == 'scw':
+            from models_converter.formats.scw import Writer
+            from models_converter.formats.scw import Parser
+        elif _from == 'scw':
+            from models_converter.formats.scw import Parser
+
+            if _to == 'obj':
+                from models_converter.formats.obj_write import Writer
+            elif _to == 'dae':
+                from models_converter.formats.dae_write import Writer
+        else:
+            from models_converter.formats.scw import Writer
+
+            if _from == 'obj':
+                from models_converter.formats.obj_read import Parser
+            elif _from == 'dae':
+                from models_converter.formats.dae_read import Parser
+
+        parser = Parser(file_data)
+
+        if _from == 'dae':
+            parser.parse_nodes()
+        elif _from == 'scw':
+            parser.split_chunks()
+        parser.parse()
+
+        parsed_data = parser.parsed
+        del file_data, parser
+
+        writer = Writer(parsed_data)
+        writen_data = writer.writen
+
+        del writer
+
+        export_file_name = f'{base_name}.{_to}'
+        export_path = f'{_to}/{export_file_name}'
+
+        mode = 'wb'
+        if _to == 'obj':
+            mode = 'w'
+        with open(export_path, mode) as export_file:
+            export_file.write(writen_data)
+            export_file.close()
+
+            _(f'{export_path} is saved!')
+
+    _('Done!')
